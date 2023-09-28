ae93f526a8d98bd2a9cbcf9adf58650af5cd12a9
XenityMus <89921366+Xenity-Mus@users.noreply.github.com>
2023-09-28 19:24:55 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -43,7 +43,7 @@ AddEventHandler('Appel:concess', function()
 	local xPlayers = ESX.GetPlayers()
 	for i = 1, #xPlayers, 1 do
 		local thePlayer = ESX.GetPlayerFromId(xPlayers[i])
-		if thePlayer.job.name == 'carshop' then
+		if thePlayer.job.name == catalogue.job then
 		TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Concessionaire', '~r~Accueil', 'Un concessionaire est appelé à l\'accueil !', 'CHAR_CARDEALER', 8)
         end
     end
@@ -73,4 +73,54 @@ end
 RegisterNetEvent('Mus:webhook')
 AddEventHandler('Mus:webhook', function(Name, Title, Description, Image, Webhook)
     DiscordWebHook(Name, Title, Description, Image, Webhook)
+end)
+
+RegisterNetEvent('Mus:cardealer:AddCar')
+AddEventHandler('Mus:cardealer:AddCar', function(hash, name, price, category)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    -- Vérifier si le modèle existe déjà dans la base de données
+    MySQL.Async.fetchScalar('SELECT COUNT(*) FROM vehicles WHERE model = @model', {
+        ['@model'] = hash
+    }, function(result)
+        local count = tonumber(result)
+
+        -- Si le modèle existe déjà, renvoyer une notification au client
+        if count > 0 then
+            TriggerClientEvent('esx:showNotification', xPlayer.source, 'Cette voiture existe déjà dans le catalogue.')
+        else
+            -- Si le modèle n'existe pas encore, l'ajouter à la base de données
+            MySQL.Async.execute('INSERT INTO vehicles (name, model, price, category) VALUES (@name, @model, @price, @category)', {
+                ['@name'] = name,
+                ['@model'] = hash,
+                ['@price'] = price,
+                ['@category'] = category
+            }, function(rowsChange)
+                TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, 'PDM', 'Ajout', 'Vous venez d\'ajouter ~o~' .. name .. ' dans le catalogue', 'CHAR_CONCESSIONNAIRE', 0)
+            end)
+        end
+    end)
+end)
+
+RegisterNetEvent('Mus:cardealer:RemoveCar')
+AddEventHandler('Mus:cardealer:RemoveCar', function(model)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    MySQL.Async.execute('DELETE FROM vehicles WHERE model = @model', {
+        ['@model'] = model
+    }, function(rowsDeleted)
+        TriggerClientEvent('esx:showNotification', xPlayer.source, 'Vous venez de supprimer : ' .. model)
+    end)
+end)
+
+RegisterNetEvent('Mus:cardealer:ChangePrice')
+AddEventHandler('Mus:cardealer:ChangePrice', function(model, price)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    MySQL.Async.execute('UPDATE vehicles SET price = @price WHERE model = @model', {
+        ['@price'] = price,
+        ['@model'] = model
+    }, function(rowsChanged)
+        TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, "PDM", 'Changement Prix', "Vous venez de changer le prix de ~o~" .. model .. "~s~ pour ~g~" .. price .. "$", "CHAR_CONCESSIONNAIRE", 0)
+    end)
 end)
