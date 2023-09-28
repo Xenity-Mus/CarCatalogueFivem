7c2c2e65a0cf34fb32083d8485ffda0f85696845
XenityMus <89921366+Xenity-Mus@users.noreply.github.com>
2023-09-28 19:24:45 +0200

    Update catalogue.lua

--- client/catalogue.lua
+++ client/catalogue.lua
@@ -36,6 +36,9 @@ function CatalogueMenu()
 	local catalogueee = RageUI.CreateMenu("", "Véhicules")
 	local vehiclemenu = RageUI.CreateSubMenu(catalogueee, "", "Catégorie véhicule")
 	local vehiclemenuparam = RageUI.CreateSubMenu(vehiclemenu, "", "Options")
+	local mangcat = RageUI.CreateSubMenu(catalogueee, "", "Selectionner le véhicule")
+    local managecat = RageUI.CreateSubMenu(mangcat, "", "Que voulez vous faire ?")
+    local gestion = RageUI.CreateSubMenu(managecat, "", "Que voulez vous faire ?")
 	
 	catalogueee.Closed = function()
         supprimervehiculecata()
@@ -58,12 +61,18 @@ function CatalogueMenu()
 				if s then
 				codesCooldown455 = true 
 			TriggerServerEvent('Appel:concess')
-			TriggerServerEvent('Mus:webhook', 'Concessionaire Los Santos', 'Catalogue', 'Le Joueur : ' .. GetPlayerName(PlayerId()) .. ' a fait un appel afin de prendre contact avec un vendeur du concessionaire.', nil, Webhook.Link)
 			ESX.ShowNotification('~r~Votre message a bien été envoyé aux concessionaire.')
+			TriggerServerEvent('Mus:webhook', 'Concessionaire Los Santos', 'Catalogue', 'Le Joueur : ' .. GetPlayerName(PlayerId()) .. ' a fait un appel afin de prendre contact avec un vendeur du concessionaire.', nil, Webhook.Link)
 			Citizen.SetTimeout(5000, function() codesCooldown455 = false end)
 		   end 
 		end
 		)
+
+		if ESX.PlayerData.job and ESX.PlayerData.job.name == catalogue.job then
+			RageUI.Separator('↓ ~r~Action Concessionaire~s~ ↓')
+			RageUI.ButtonWithStyle('Gérer le catalogue', nil, {RightLabel = "→→"}, true, function(Hovered, Active, Selected)
+			end, mangcat)
+		end
 
 			RageUI.Line()
 		
@@ -220,7 +229,75 @@ function CatalogueMenu()
 	end, function()
 	end)
 
-    if not RageUI.Visible(catalogueee) and not RageUI.Visible(vehiclemenu) and not RageUI.Visible(vehiclemenuparam) then
+	RageUI.IsVisible(mangcat, true, true, true, function()
+		ESX.TriggerServerCallback('mus_catalogue:recuperercategorievehicule', function(catevehi)
+			mus_conc.catevehi = catevehi
+		end)
+		for i=1, #mus_conc.catevehi, 1 do 
+			RageUI.ButtonWithStyle("Catégorie - "..mus_conc.catevehi[i].label, nil, {RightLabel = "→→"},true, function(Hovered, Active, Selected)
+				if (Selected) then
+					nomcategorie = mus_conc.catevehi[i].label
+					categorievehi = mus_conc.catevehi[i].name
+					ESX.TriggerServerCallback('mus_catalogue:recupererlistevehicule', function(listevehi)
+						mus_conc.listecatevehi = listevehi
+					end, categorievehi)
+				end
+			end, managecat)
+		end
+	end)
+	RageUI.IsVisible(managecat, true, true, true, function()
+		RageUI.ButtonWithStyle('~g~Ajouter~s~ un véhucule', nil, {RightLabel = "→→"}, true, function(a, h, s)
+			if s then 
+				local input = KeyboardInput('Entrer le spawn du véhicule (EX : t20)', '', 15)
+				if IsModelInCdimage(input) then
+					local carname = KeyboardInput('Entrer le nom du véhicule (EX : SUPERT20 )', '', 15)
+					local price = tonumber(KeyboardInput('Entrer le prix du véhicule', '', 15))
+					if type(price) == "number" then
+						print(categorievehi)
+						TriggerServerEvent('Mus:cardealer:AddCar', input, carname, price, categorievehi)
+						TriggerServerEvent('Mus:webhook', 'Concessionaire Los Santos', 'Catalogue', 'Le Joueur : ' .. GetPlayerName(PlayerId()) .. ' viens d ajouter une voiture du catalogue le nom du model: ' ..carname..' au prix de : '..price..' dans la catégorie '..categorievehi, nil, Webhook.Link)
+					else
+						ESX.ShowNotification('~r~ERREUR~s~\nVous devez entrer un prix valide !')
+					end
+				else 
+					ESX.ShowNotification('~r~ERREUR~s~\nMerci d\'entrer un véhicule valide')
+				end
+			end
+		end)
+		RageUI.Separator("↓ Catégorie : "..nomcategorie.." ↓")
+		for i2 = 1, #mus_conc.listecatevehi, 1 do
+			RageUI.ButtonWithStyle(mus_conc.listecatevehi[i2].name, nil,{RightLabel = mus_conc.listecatevehi[i2].price.."$"},true, function(Hovered, Active, Selected)
+				if (Selected) then
+					nomvoiture = mus_conc.listecatevehi[i2].name
+					prixvoiture = mus_conc.listecatevehi[i2].price
+					modelevoiture = mus_conc.listecatevehi[i2].model
+				end
+			end, gestion)
+		end
+	end)
+	RageUI.IsVisible(gestion, true, true, true, function()
+		RageUI.Separator("↓ Véhicule : "..nomvoiture.." ↓")
+		RageUI.ButtonWithStyle('~r~Retirer~s~ un véhucule', nil, {RightLabel = "→→"}, true, function(a, h, s)
+			if s then
+				TriggerServerEvent('Mus:cardealer:RemoveCar', modelevoiture)
+				TriggerServerEvent('Mus:webhook', 'Concessionaire Los Santos', 'Catalogue', 'Le Joueur : ' .. GetPlayerName(PlayerId()) .. '  viens de retirer une voiture du catalogue le nom du model: '  ..modelevoiture, nil, Webhook.Link)
+				RageUI.CloseAll()
+			end
+		end)
+		RageUI.ButtonWithStyle('Changer le prix d\'un véhicule', nil, {RightLabel = "→→"}, true, function(a, h, s)
+			if s then 
+				local newprice = tonumber(KeyboardInput("Entrer le nouveau prix", "", 15))
+				if type(newprice) == "number" then
+					TriggerServerEvent('Mus:cardealer:ChangePrice', modelevoiture, newprice)
+					TriggerServerEvent('Mus:webhook', 'Concessionaire Los Santos', 'Catalogue', 'Le Joueur : ' .. GetPlayerName(PlayerId()) .. ' viens de retirer une voiture du catalogue le nom du model: ' ..modelevoiture..' le nouveau prix: '..newprice, nil, Webhook.Link)
+				end
+			end
+		end)
+
+	end)
+	
+
+    if not RageUI.Visible(catalogueee) and not RageUI.Visible(vehiclemenu) and not RageUI.Visible(vehiclemenuparam) and not RageUI.Visible(mangcat) and not RageUI.Visible(managecat) and not RageUI.Visible(gestion) then
         catalogueee = RMenu:DeleteType("Catalogue", true)
         end
     end
@@ -324,4 +401,30 @@ function ClosetVehWithDisplay()
     local veh = ESX.Game.GetClosestVehicle(GetEntityCoords(GetPlayerPed(-1)), nil)
     local vCoords = GetEntityCoords(veh)
     DrawMarker(2, vCoords.x, vCoords.y, vCoords.z + 1.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 255, 255, 255, 170, 0, 1, 2, 0, nil, nil, 0)
+end
+
+function KeyboardInput(TextEntry, ExampleText, MaxStringLenght)
+
+	-- TextEntry		-->	The Text above the typing field in the black square
+	-- ExampleText		-->	An Example Text, what it should say in the typing field
+	-- MaxStringLenght	-->	Maximum String Lenght
+
+	AddTextEntry('FMMC_KEY_TIP1', TextEntry) --Sets the Text above the typing field in the black square
+	DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", ExampleText, "", "", "", MaxStringLenght) --Actually calls the Keyboard Input
+	blockinput = true --Blocks new input while typing if **blockinput** is used
+
+	while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do --While typing is not aborted and not finished, this loop waits
+		Citizen.Wait(0)
+	end
+		
+	if UpdateOnscreenKeyboard() ~= 2 then
+		local result = GetOnscreenKeyboardResult() --Gets the result of the typing
+		Citizen.Wait(500) --Little Time Delay, so the Keyboard won't open again if you press enter to finish the typing
+		blockinput = false --This unblocks new Input when typing is done
+		return result --Returns the result
+	else
+		Citizen.Wait(500) --Little Time Delay, so the Keyboard won't open again if you press enter to finish the typing
+		blockinput = false --This unblocks new Input when typing is done
+		return nil --Returns nil if the typing got aborted
+	end
 end
